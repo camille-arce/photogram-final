a326f4730bd79c3f6f0b29b5f58c24831d0b5b27
camille-arce <117893473+camille-arce@users.noreply.github.com>
2023-05-01 18:29:17 +0000

    added NAV

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -16,9 +16,9 @@ class Photo < ApplicationRecord
 
   has_many(:photo_comments, { :class_name => "Comment", :foreign_key => "photo_id"})
 
-  belongs_to(:poster { :class_name => "User", :foreign_key => "owner_id" })
+  belongs_to(:poster, { :class_name => "User", :foreign_key => "owner_id" })
 
-  has_many(:photo_to_likes, { :class_name => "Like", :foreign_key => "photo_id" )
+  has_many(:photo_to_likes, { :class_name => "Like", :foreign_key => "photo_id" })
 
   has_many(:fans, { :through => "photo_to_likes", :source => "like_to_user" })
 end
