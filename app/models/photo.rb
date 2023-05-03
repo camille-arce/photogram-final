f85e1d1d7139923016c228fc0c59a6664f8b03b9
camille-arce <117893473+camille-arce@users.noreply.github.com>
2023-05-03 17:27:38 +0000

    added scope in accept sent follow requests

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -21,9 +21,9 @@ class Photo < ApplicationRecord
   has_many(:photo_to_likes, { :class_name => "Like", :foreign_key => "photo_id", :dependent => :destroy })
 
   #indirect associations
-  has_many(:fans, { :through => "photo_to_likes", :source => "like_to_user" })
-  #has_many(:followers, { :through => :poster, :source => :following })
-  #has_many(:fan_followers, { :through => :fans, :source => :following })
+  has_many(:fans, { :through => :photo_to_likes, :source => :like_to_user })
+  has_many(:followers, { :through => :poster, :source => :following })
+  has_many(:fan_followers, { :through => :fans, :source => :following })
   
   mount_uploader(:image, ImageUploader)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,15 +21,18 @@ class User < ApplicationRecord
   has_many(:comments, { :class_name => "Comment", :foreign_key => "author_id", :dependent => :destroy })
   has_many(:own_photos, { :class_name => "Photo", :foreign_key => "owner_id" })
   has_many(:sent_follow_requests, { :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy })
+  has_many(:accepted_sent_follow_requests, -> { where(status: "accepted") }, { :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy })
   has_many(:received_follow_requests, { :class_name => "FollowRequest", :foreign_key => "recipient_id", :dependent => :destroy })
   # Created so that an indirect association from User to Photos can be made - through "user_likes" / :through - should be in the same model as the first table were are coming from
   has_many(:user_likes, { :class_name => "Like", :foreign_key => "fan_id", :dependent => :destroy })
 
+  
   #Indirect associations
-  has_many(:following, { :through => :sent_follow_requests, :source => :recipient })
+  has_many(:following, { :through => :accepted_sent_follow_requests, :source => :recipient })
   has_many(:followers, { :through => :received_follow_requests, :source => :sender })
   has_many(:liked_photos, { :through => :user_likes, :source => :like_to_photos })
-  #has_many(:feed, { :through => :following, :source => :own_photos })
+
+  has_many(:feed, { :through => :following, :source => :own_photos })
   #has_many(:activity, { :through => :following, :source => :liked_photos })
 
   def follow_request_for(other_user)
